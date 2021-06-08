fe052479837cd42473960dd0523295464baf7ca2
Roc <1344471553@qq.com>
2021-06-08 15:49:48 +0800

    First Commit 2021年6月8日15:49:29

---
+++ src/main/java/com/roc/study/SuperMasterApplication.java
@@ -0,0 +1,13 @@
+package com.roc.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/roc/study/SuperMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roc.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
