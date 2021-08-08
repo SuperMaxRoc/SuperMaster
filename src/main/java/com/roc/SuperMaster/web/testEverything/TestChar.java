94af65de3577769fe052d2f08f4f5d956b603135
Roc <1344471553@qq.com>
2021-08-08 23:14:23 +0800

    2021年8月8日23:14:01

---
+++ src/main/java/com/roc/SuperMaster/web/testEverything/TestChar.java
@@ -0,0 +1,22 @@
+package com.roc.SuperMaster.web.testEverything;
+
+import org.junit.Test;
+
+/**
+ * @Author: WP
+ * @Date: 2021/8/8 23:03
+ * @Version 1.0
+ * @ClassName TestChar.java
+ * @Description testChar
+ * @UpdateUser WP
+ */
+public class TestChar {
+    @Test
+    public void testChar(){
+        char uniChar = 0x0041;
+        System.out.println(uniChar);
+        Integer integer = 55;
+        String s = "A";
+        System.out.println(s);
+    }
+}
