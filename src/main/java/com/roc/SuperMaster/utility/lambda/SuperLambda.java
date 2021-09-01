c6acbb186c2405af82fbe63e54919859b62d9f3f
Roc <1344471553@qq.com>
2021-09-01 17:48:14 +0800

    2021年9月1日17:47:07

---
+++ src/main/java/com/roc/SuperMaster/utility/lambda/SuperLambda.java
@@ -0,0 +1,12 @@
+package com.roc.SuperMaster.utility.lambda;
+
+/**
+ * @Author Roc
+ * @Date 2021/9/1 17:48
+ * @Version 1.0.0
+ * @ClassName SuperLambda.java
+ * @Description ToDo
+ * @UpdateUser Roc
+ */
+public class SuperLambda {
+}
