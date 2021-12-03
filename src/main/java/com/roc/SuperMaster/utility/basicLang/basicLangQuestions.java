269e9e929e9bb96e3f6cb8f04efea4237dc2e3b4
Roc <1344471553@qq.com>
2021-12-03 11:22:21 +0800

    java ==和Equals()方法对比及测试 2021年12月3日11:21:39

--- src/main/java/com/roc/SuperMaster/utility/basicLang/basicLangQuestions.java
+++ src/main/java/com/roc/SuperMaster/utility/basicLang/basicLangQuestions.java
@@ -12,7 +12,7 @@ import java.math.BigDecimal;
  * @Description java基础语言包中有趣的问题
  * @UpdateUser Roc
  */
-public class basicLangQuestions {
+public class basicLangQuestions extends Object {
 
     /**
      * @param
@@ -48,6 +48,149 @@ public class basicLangQuestions {
         BigDecimal bigDecimal02 = BigDecimal.valueOf(0.2);
         System.out.println(bigDecimal01.equals(bigDecimal));
         System.out.println(a == b);
+    }
+
+    /**
+     * @param
+     * @return void
+     * @Author: WP
+     * @Date: 2021/12/3 9:26
+     * @Version 1.0
+     * @Description: 一次性解决==和equals()方法的区别，同时针对面试整理的面试题01
+     * @UpdateUser WP
+     */
+    @Test
+    public void testEquals01() {
+        /*
+        == vs equals()
+        区别：
+        1.== 关系运算符 equals()是Object类的方法。
+        2.== 既可以比较基本类型，也可以比较引用类型，equals()仅可以比较引用类型。
+            == 比较基本类型时直接对比目标的值，并返回结果
+               比较引用类型，对比目标的内存地址，并返回结果
+            equals()比较引用类型，默认是判断目标对象的内存地址，实际生产情况下指对比包装类及String类，因为上述类重写了继承Object类的方法，所以是对比目标对象的值
+         */
+
+        /**
+         * 此处难点容易混淆的点也是 整数缓冲区和字符串常量池 的应用
+         */
+
+        int a = 100;
+        int b = 100;
+        int c = 200;
+        int d = 200;
+        System.out.println("--------------------------");
+        System.out.println("== 对比基本类型");
+        //true
+        System.out.println(a == b);
+        //true
+        System.out.println(c == d);
+        System.out.println("--------------------------");
+        Integer integer01 = 100;
+        Integer integer02 = 100;
+        Integer integer03 = 200;
+        Integer integer04 = 200;
+        System.out.println("== 对比基本类型的包装类(一)");
+        //true
+        System.out.println(integer01 == integer02);
+        //false
+        System.out.println(integer03 == integer04);
+        System.out.println("--------------------------");
+        System.out.println("== 对比基本类型的包装类(二)");
+        Integer integer05 = new Integer(100);
+        Integer integer06 = new Integer(100);
+        Integer integer07 = new Integer(200);
+        Integer integer08 = new Integer(200);
+        //false
+        System.out.println(integer05 == integer06);
+        //false
+        System.out.println(integer05 == integer01);
+        //false
+        System.out.println(integer07 == integer08);
+        //false
+        System.out.println(integer07 == integer04);
+        System.out.println("--------------------------");
+        System.out.println("== 对比String类");
+        String str01 = "Hello";
+        String str02 = "He";
+        String str03 = "llo";
+        String str04 = str02 + str03;
+        String str05 = "He" + "llo";
+        String str06 = "He" + new String("llo");
+        String str07 = new String("Hello");
+        String str08 = new String("He") + new String("llo");
+        //false
+        System.out.println(str01 == str04);
+        //true
+        System.out.println(str01 == str05);
+        //false
+        System.out.println(str04 == str05);
+        //false
+        System.out.println(str01 == str06);
+        //false
+        System.out.println(str01 == str07);
+        //false
+        System.out.println(str01 == str08);
+        //false
+        System.out.println(str07 == str08);
+        System.out.println("--------------------------");
+    }
+    
+    /**
+     * @Author: WP
+     * @Date: 2021/12/3 10:49
+     * @Version 1.0
+     * @Description: 一次性解决==和equals()方法的区别，同时针对面试整理的面试题02
+     * @UpdateUser WP
+     * @param  
+     * @return void
+     */
+    @Test
+    public void testEquals02(){
+        Integer integer01 = 100;
+        Integer integer02 = 100;
+        Integer integer03 = 200;
+        Integer integer04 = 200;
+        System.out.println("equals 判断基本类型的包装类01");
+        //true
+        System.out.println(integer01.equals(integer02));
+        //true
+        System.out.println(integer03.equals(integer04));
+
+        System.out.println("--------------------------");
+        System.out.println("equals 判断基本类型的包装类02");
+        Integer integer05 = new Integer(100);
+        Integer integer06 = new Integer(100);
+        Integer integer07 = new Integer(200);
+        Integer integer08 = new Integer(200);
+        //true
+        System.out.println(integer05.equals(integer06));
+        //true
+        System.out.println(integer07.equals(integer08));
+
+        System.out.println("--------------------------");
+        System.out.println("equals 判断String类");
+        String str01 = "Hello";
+        String str02 = "He";
+        String str03 = "llo";
+        String str04 = str02 + str03;
+        String str05 = "He" + "llo";
+        String str06 = "He" + new String("llo");
+        String str07 = new String("Hello");
+        String str08 = new String("He") + new String("llo");
+        //true
+        System.out.println(str01.equals(str04));
+        //true
+        System.out.println(str01.equals(str05));
+        //true
+        System.out.println(str01.equals(str06));
+        //true
+        System.out.println(str01.equals(str07));
+        //true
+        System.out.println(str01.equals(str08));
+        //true
+        System.out.println(str07.equals(str08));
+        System.out.println("--------------------------");
     }
 
 }
